804b2cf2bc7fce229525bd30f357fd2da3bbb3d1
Alexander Wolf <alexander.wolf@8mylez.com>
2020-07-01 14:15:09 +0200

    Change tax rate to 16 because of corona

--- Components/Order/Factory/OrderFactory.php
+++ Components/Order/Factory/OrderFactory.php
@@ -116,7 +116,7 @@ class OrderFactory
 
         if ($orderStruct->getNetOrder()) {
             $shippingCosts = 0.0;
-            $shippingCosts = $orderStruct->getShippingCostsNet() * 1.19;
+            $shippingCosts = $orderStruct->getShippingCostsNet() * 1.16;
             $order->setInvoiceShipping($shippingCosts);
         }
 
